b7753878414c4a90a232870344a82deb39391dc9
nikolic.cs@gmail.com <nikolic.cs@gmail.com>
2021-02-13 16:00:09 +0100

    Izrada

---
+++ main.js
@@ -0,0 +1,21 @@
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 50){
+        $('.sticky').addClass('stickyAdd');
+    }else{
+        $('.sticky').removeClass('stickyAdd');
+    }
+});
+///
+const naslov='Izrada sajta';
+let i = 0;
+
+const kuckanje = () => {
+     if(i < naslov.length){
+         document.querySelector('.tipkajuciNaslov').innerHTML += naslov.charAt(i);
+         i++;
+         
+         setTimeout(kuckanje, 150);
+     }
+ }
+ kuckanje();
